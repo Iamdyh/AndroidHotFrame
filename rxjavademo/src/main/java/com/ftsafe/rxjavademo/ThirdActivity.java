11c04dfb3676ff1d759824d198136ca8f7536d79
dyh <15576567913@163.com>
2019-01-02 14:54:09 +0800

    学习RxJava2中zip

--- rxjavademo/src/main/java/com/ftsafe/rxjavademo/ThirdActivity.java
+++ rxjavademo/src/main/java/com/ftsafe/rxjavademo/ThirdActivity.java
@@ -1,5 +1,6 @@
 package com.ftsafe.rxjavademo;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +14,10 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -86,30 +90,109 @@ public class ThirdActivity extends AppCompatActivity {
 //          });
 
         //concatMap
-        Observable.create(new ObservableOnSubscribe<Integer>() {
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) {
+//                emitter.onNext(1);
+//                emitter.onNext(2);
+//                emitter.onNext(3);
+//            }
+//        }).concatMap(new Function<Integer, ObservableSource<String>>() {
+//            @Override
+//            public ObservableSource<String> apply(Integer integer) throws Exception {
+//                final List<String> list = new ArrayList<>();
+//                for(int i = 0; i < 3; i++){
+//                    list.add("I am value " + integer);
+//                }
+//                return Observable.fromIterable(list).delay(10, TimeUnit.SECONDS);
+//            }
+//        }).subscribeOn(Schedulers.newThread())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<String>() {
+//                    @Override
+//                    public void accept(String s) throws Exception {
+//                        Log.e(TAG, "accept: " + s);
+//                        mTvInfo.setText(s);
+//                    }
+//                });
+
+
+        //zip
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
-            public void subscribe(ObservableEmitter<Integer> emitter) {
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.e(TAG, "emit: 1");
                 emitter.onNext(1);
+                SystemClock.sleep(1000);
+
+                Log.e(TAG, "emit: 2");
                 emitter.onNext(2);
+                SystemClock.sleep(1000);
+
+                Log.e(TAG, "emit: 3");
                 emitter.onNext(3);
+                SystemClock.sleep(1000);
+
+                Log.e(TAG, "emit: 4");
+                emitter.onNext(4);
+                SystemClock.sleep(1000);
+
+                Log.e(TAG, "complete 1");
+                emitter.onComplete();
+
+
+
             }
-        }).concatMap(new Function<Integer, ObservableSource<String>>() {
+        }).subscribeOn(Schedulers.io());
+
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
-            public ObservableSource<String> apply(Integer integer) throws Exception {
-                final List<String> list = new ArrayList<>();
-                for(int i = 0; i < 3; i++){
-                    list.add("I am value " + integer);
-                }
-                return Observable.fromIterable(list).delay(10, TimeUnit.SECONDS);
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                Log.e(TAG, "emit: A");
+                emitter.onNext("A");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "emit: B");
+                emitter.onNext("B");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "emit: C");
+                emitter.onNext("C");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "complete 2");
+                emitter.onComplete();
+
             }
-        }).subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String s) throws Exception {
-                        Log.e(TAG, "accept: " + s);
-                        mTvInfo.setText(s);
-                    }
-                });
+        }).subscribeOn(Schedulers.io());
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            @Override
+            public String apply(Integer integer, String s) {
+                return integer + s;
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+          .subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e(TAG, "onNext: " + s);
+//                mTvInfo.setText(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "onComplete: ");
+            }
+        });
     }
 }
